147d6df62ed7e39ee92189a2a39698278ace3637
Khaldir <jaketrigg@gmail.com>
2017-01-18 16:44:36 +0000

    discoverPeers added

--- RPTool/app/src/main/java/com/example/khaldir/rptool/MainActivity.java
+++ RPTool/app/src/main/java/com/example/khaldir/rptool/MainActivity.java
@@ -3,10 +3,19 @@ package com.example.khaldir.rptool;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static android.R.attr.action;
 import static android.os.Looper.getMainLooper;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     WifiP2pManager.Channel mChannel;
     BroadcastReceiver mReceiver;
     IntentFilter mIntentFilter;
+    WifiP2pManager.PeerListListener myPeerListListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+
+
+
+
     }
 
     /* register the broadcast receiver with the intent values to be matched */
@@ -44,6 +59,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         unregisterReceiver(mReceiver);
+    }
+
+    protected void discoverPeers(){
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener(){
+            @Override
+            public void onSuccess() {
+                if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+                    // request available peers from the wifi p2p manager. This is an
+                    // asynchronous call and the calling activity is notified with a
+                    // callback on PeerListListener.onPeersAvailable()
+                    if (mManager != null) {
+                        mManager.requestPeers(mChannel, myPeerListListener);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+
+            }
+        });
+
+        WifiP2pDeviceList peers = new WifiP2pDeviceList();
+        myPeerListListener.onPeersAvailable(peers);
+
+        Collection<WifiP2pDevice> devices = peers.getDeviceList();
+        Spinner deviceList = (Spinner)findViewById(R.id.deviceList);
+        List<String> arraySpinner = new ArrayList<String>();
+
+        for (WifiP2pDevice device:devices)
+        {
+            arraySpinner.add(device.toString());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arraySpinner);
+
     }
 
 }
